3536dbe38b7ffac3161988f16f6855094ef79250
noyciy7037 <noyciy7037@gmail.com>
2026-03-07 22:11:42 +0900

    fix link

--- link_repo.sh
+++ link_repo.sh
@@ -1,12 +1,14 @@
+# Have to run before cd
+script_dir=$(cd $(dirname $0); pwd)
 cd media
-git_dir=translation_repository/`echo "https://github.com/noyciy7037/kde-translate-demo.git" | cut -d ":" -f 2 | cut -d "/" -f 4-`
+git_dir=_translation_repository/`echo "https://github.com/noyciy7037/kde-translate-demo.git" | cut -d ":" -f 2 | cut -d "/" -f 4-`
 git_parent=`echo $git_dir | rev | cut -d "/" -f 2- | rev`
 git_root=`echo $git_parent | cut -d "/" -f 1`
 mkdir -p $git_parent
-ln -s `pwd`/`dirname $0` $git_dir
+ln -s $script_dir $git_dir
 mkdir projects
 
-for file in ja/summit/messages/*
+for file in $script_dir/ja/summit/messages/*
 do
   project_name=`echo "${file}" | rev | cut -d "/" -f 1 | rev`
   ln -s `pwd`/$git_root projects/$project_name
